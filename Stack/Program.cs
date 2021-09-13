c0ec9f246803354750fc0cc1aeab9ff828cd207e
B2B\bureap <bureap@b2b.regn.net>
2021-09-13 12:07:21 +0100

    AAsdasd

---
+++ Stack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack _stack = new Stack();
+
+            int ini = 0;
+            int max = 5;
+
+            for (int i = ini; i < -max; i++)
+                _stack.Push(i);
+            
+            for(int i = ini; i <= max;i++)
+                Console.WriteLine(_stack.Pop());
+
+            Console.ReadLine();
+
+        }
+    }
+}
